2cc7db13d8c78c23b30464eab724f7d65299fc59
Paul grossman <qtpmgrossman@gmail.com>
2018-06-16 17:42:06 -0500

    Initial Git Push

---
+++ NuclearCodes/src/NuclearCodeChecker.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+
+//one class needs to have a main() method
+public class NuclearCodeChecker
+{
+	String[] theArray;
+	
+	int arraySize = 30;
+    int itemsInArray = 0;
+ 
+	
+public static void main(String[] args){
+    
+    
+	
+	
+    HashFunction theFunc = new HashFunction(arraySize * 2);
+	
+    
+ 	theFunc.DisplayTheStack();
+ 
+ 	
+	//Create a list of Nuclear codes
+	 for(int n=0; n < StringsForarray.length;n++) {
+		 
+		 String newElementVal = stringsForArray[n];
+		 
+		 int arrayIndex = Integer.parseInt((newElementVal) % 29;
+		 
+		 //while()
+		 
+	 }
+	
+	 
+	 HashFunction(int size){
+		 arraySize = size;
+		theArray = new String[size]
+				
+	 }
+	
+	//Store in Dictionary
+
+ 
+	//for each code reverse the code
+ 	
+ 
+	//check the list for this code
+	
+	//if found increment the number of active codes
+	
+	//Report the number of codes found
+	
+ 
+}
+
+
+public static String ReverseString (String sInput) {
+	 return new StringBuilder(sInput).reverse().toString();
+	 }
+
+
+}
